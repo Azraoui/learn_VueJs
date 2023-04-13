c77e32728e1fac9c7376402b63e3f1224764707a
Azraoui <ael-azra@student.1337.ma>
2023-04-13 00:09:46 +0000

    add v-if for display message only when password length greater than onde

--- vueFirstProject/src/App.vue
+++ vueFirstProject/src/App.vue
@@ -1,17 +1,38 @@
 <template>
+    <NavBar />
     <h1>{{ title }}</h1>
+    <input type="password" @input="validatePassword" v-model="password" />
+    <p v-if="password.length > 0" :style="{ color: msgColor }">{{ message }}</p>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                title: "Hello, World!"
+import NavBar from './components/NavBar.vue';
+export default {
+    data() {
+        return {
+            title: "Password Validate!",
+            message: "",
+            msgColor: "",
+            password: ""
+        };
+    },
+    methods: {
+        validatePassword() {
+            const password = this.password;
+            const isValid = /[a-zA-z]/.test(password)
+                && /\d/.test(password) && password.length >= 8;
+            if (isValid) {
+                this.message = "Password Valid";
+                this.msgColor = "green";
             }
-        },
-    }
+            else {
+                this.message = "Password Invalid";
+                this.msgColor = "red";
+            }
+        }
+    },
+    components: { NavBar }
+}
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
---
+++ vueFirstProject/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav></nav>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
+
+<style scoped>
+
+</style>
